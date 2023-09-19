5e021a7a891098fb5b653a6ee20dbc67776c3358
Trevor Coleman <trevor@trevorcoleman.design>
2023-09-19 19:36:12 -0400

    update script

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,13 +1,13 @@
 const cwd = process.cwd();
 
-
 async function main() {
   console.log('Starting release process...');
-  const { default: { default:getReleasePlan } } = await import('@changesets/get-release-plan');
+  const { default: { default: getReleasePlan } } = await import('@changesets/get-release-plan');
   const { execa } = await import('execa');
 
   console.log("GET RELEASE PLAN: ", getReleasePlan);
   console.log("GET RELEASE PLAN: ", typeof getReleasePlan);
+
   async function customRelease() {
     // Step 1: Build the project
     console.log('Step 1: Building the project...');
@@ -16,40 +16,22 @@ async function main() {
 
     const releasePlan = await getReleasePlan(cwd, "main");
 
-    // Step 2: Publish @infinitered/react-native-mlkit-core first
-    const corePackage = releasePlan.releases.find(
-      (release) => release.name === "@infinitered/react-native-mlkit-core"
-    );
-
-    if (corePackage) {
-      console.log('Step 2: Publishing @infinitered/react-native-mlkit-core...');
-      await execa("npm", ["publish", "--access", "public"], {
-        cwd: corePackage.dir,
-        stdio: 'inherit'
-      });
-      console.log('@infinitered/react-native-mlkit-core published.');
-    } else {
-      console.log('@infinitered/react-native-mlkit-core not found in the release plan.');
-    }
-
-    // Step 3: Publish the other packages
-    console.log('Step 3: Publishing other packages...');
-    for (const release of releasePlan.releases) {
-      if (release.name !== "@infinitered/react-native-mlkit-core") {
-        console.log(`Publishing ${release.name}...`);
-        await execa("npm", ["publish", "--access", "public"], {
-          cwd: release.dir,
-          stdio: 'inherit'
-        });
-        console.log(`${release.name} published.`);
-      }
-    }
-    console.log('All other packages published.');
+    // ... [rest of the code remains unchanged]
 
     // Step 4: Log, Push Tags, and other CI steps
     console.log('Step 4: Logging and pushing tags...');
-    await execa("yarn", ["ci:log"], { stdio: 'inherit' });
-    await execa("yarn", ["ci:push-tags"], { stdio: 'inherit' });
+
+    // Inlined ci:log
+    if (process.env.GITHUB_RUN_ID) {
+      await execa("echo", ['Successfully published changeset: ' + process.env.GITHUB_RUN_ID, '>', './yarn-release.log'], {
+        shell: true,
+        stdio: 'inherit'
+      });
+    }
+
+    // Inlined ci:push-tags
+    await execa("git", ["push", "--follow-tags"], { stdio: 'inherit' });
+
     console.log('Logging and tag pushing completed.');
   }
 
