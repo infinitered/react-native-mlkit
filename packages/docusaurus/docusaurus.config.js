efa568a9d7650f05b7436b3e04b08e2f11749d86
Trevor Coleman <trevor@trevorcoleman.design>
2023-09-29 14:08:39 -0400

    docs: set favicon

--- packages/docusaurus/docusaurus.config.js
+++ packages/docusaurus/docusaurus.config.js
@@ -1,123 +1,122 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/okaidia');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/okaidia");
 
-/** @type {import('@docusaurus/types').Config} */
+/** @type {import("@docusaurus/types").Config} */
 const config = {
-  title: 'react-native-mlkit',
-  tagline: 'Run MLKit and TFLite Models on-device for fast, low-cost inference',
-  favicon: 'img/favicon.ico',
+  title: "react-native-mlkit",
+  tagline: "Run MLKit and TFLite Models on-device for fast, low-cost inference",
+  favicon: "img/favicon.svg",
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: "https://your-docusaurus-test-site.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/react-native-mlkit/',
+  baseUrl: "/react-native-mlkit/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'infinitered', // Usually your GitHub org/user name.
-  projectName: 'react-native-mlkit', // Usually your repo name.
+  organizationName: "infinitered", // Usually your GitHub org/user name.
+  projectName: "react-native-mlkit", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
   trailingSlash: true,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
+      "classic",
+      /** @type {import("@docusaurus/preset-classic").Options} */
       ({
         docs: {
-          routeBasePath:"/",
-          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
         },
         theme: {
-            customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
-
       }),
     ],
   ],
 
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    /** @type {import("@docusaurus/preset-classic").ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'react-native-mlkit',
+        title: "react-native-mlkit",
         logo: {
-          alt: 'Infinite Red Logo',
-          src: 'img/logo.svg',
+          alt: "Infinite Red Logo",
+          src: "img/logo.svg",
         },
         items: [
-            {
-            type: 'docSidebar',
-            sidebarId: 'docs',
-            position: 'left',
-            label: 'Docs',
+          {
+            type: "docSidebar",
+            sidebarId: "docs",
+            position: "left",
+            label: "Docs",
           },
 
           {
-            type: 'docSidebar',
-            sidebarId: 'contribute',
-            position: 'left',
-            label: 'Contribute',
+            type: "docSidebar",
+            sidebarId: "contribute",
+            position: "left",
+            label: "Contribute",
           },
           {
-            href: 'https://github.com/infinitered/react-native-mlkit',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/infinitered/react-native-mlkit",
+            label: "GitHub",
+            position: "right",
           },
         ],
-        style: 'dark'
+        style: "dark",
       },
       footer: {
-        style: 'light',
+        style: "light",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Docs',
-                to: '/docs/introduction',
+                label: "Docs",
+                to: "/docs/introduction",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/react-native-mlkit',
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/react-native-mlkit",
               },
               {
-                label: 'Slack Community',
-                href: 'https://community.infinite.red',
+                label: "Slack Community",
+                href: "https://community.infinite.red",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/infinitered',
+                label: "Twitter",
+                href: "https://twitter.com/infinitered",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus",
               },
             ],
           },
@@ -132,5 +131,3 @@ const config = {
 };
 
 module.exports = config;
-
-
