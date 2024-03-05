4f2193b942e283fc437aebf254e5f961577607c0
Frank Calise <fcalise@gmail.com>
2024-03-05 10:33:37 -0500

    package boilerplate updates

---
+++ modules/react-native-mlkit-document-scanner/src/index.ts
@@ -0,0 +1,42 @@
+import {
+  NativeModulesProxy,
+  EventEmitter,
+  Subscription,
+} from "expo-modules-core";
+
+// Import the native module. On web, it will be resolved to RNMLKitDocumentScannerModule.web.ts
+// and on native platforms to RNMLKitDocumentScannerModule.ts
+import RNMLKitDocumentScannerModule from "./module/RNMLKitDocumentScannerModule";
+import {
+  ChangeEventPayload,
+  RNMLKitDocumentScannerModuleViewProps,
+} from "./module/RNMLKitDocumentScannerModule.types";
+import RNMLKitDocumentScannerModuleView from "./module/RNMLKitDocumentScannerModuleView";
+
+// Get the native constant value.
+export const PI = RNMLKitDocumentScannerModule.PI;
+
+export function hello(): string {
+  return RNMLKitDocumentScannerModule.hello();
+}
+
+export async function setValueAsync(value: string) {
+  return await RNMLKitDocumentScannerModule.setValueAsync(value);
+}
+
+const emitter = new EventEmitter(
+  RNMLKitDocumentScannerModule ??
+    NativeModulesProxy.RNMLKitDocumentScannerModule
+);
+
+export function addChangeListener(
+  listener: (event: ChangeEventPayload) => void
+): Subscription {
+  return emitter.addListener<ChangeEventPayload>("onChange", listener);
+}
+
+export {
+  RNMLKitDocumentScannerModuleView,
+  type RNMLKitDocumentScannerModuleViewProps,
+  type ChangeEventPayload,
+};
