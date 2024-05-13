c1ea2994b22a4f3457631ef597db40be18b988a8
Frank Calise <fcalise@gmail.com>
2024-05-13 11:49:39 -0400

    fix(example-app): missing React import

--- apps/ExampleApp/app/screens/HomeScreen/components/DemoListItem.tsx
+++ apps/ExampleApp/app/screens/HomeScreen/components/DemoListItem.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import { Text } from "~/components"
 import { DemoInfo } from "../demoInfo"
 import { ViewStyle, Pressable, View, ImageStyle, TextStyle } from "react-native"
