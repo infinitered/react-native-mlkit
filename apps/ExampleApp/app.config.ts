14fd0797703d0506ad7df1fa8d68ba89b6df0ef8
Mazen Chami <mazenchami@gmail.com>
2024-06-21 10:23:43 -0400

    reverting work done on app.json file--will auto incrament a different way

--- apps/ExampleApp/app.config.ts
+++ apps/ExampleApp/app.config.ts
@@ -1,8 +1,6 @@
 import "ts-node/register"
 import { ExpoConfig, ConfigContext } from "@expo/config"
 
-const BUILD_NUMBER = 1
-
 /**
  * Use ts-node here so we can use TypeScript for our Config Plugins
  * and not have to compile them to JavaScript
@@ -15,87 +13,10 @@ const { withFlipperDisabled } = require("./plugins/withFlipperDisabled")
  * You can read more about Expo's Configuration Resolution Rules here:
  * https://docs.expo.dev/workflow/configuration/#configuration-resolution-rules
  */
-module.exports = ({ config }: ConfigContext): Partial<ExpoConfig> => ({
-  ...config,
-  name: "React Native MLKit",
-  slug: "react-native-mlkit",
-  owner: "infinitered",
-  extra: {
-    eas: {
-      projectId: "4faa9328-e941-4395-879c-f558bf07e678",
-    },
-  },
-  scheme: "react-native-mlkit",
-  version: "1.0.0",
-  orientation: "portrait",
-  icon: "./assets/images/app-icon-all.png",
-  splash: {
-    image: "./assets/images/splash-logo-all.png",
-    resizeMode: "contain",
-    backgroundColor: "#F4F2F1",
-  },
-  updates: {
-    fallbackToCacheTimeout: 0,
-  },
-  jsEngine: "hermes",
-  assetBundlePatterns: ["**/*"],
-  android: {
-    icon: "./assets/images/app-icon-android-legacy.png",
-    package: "red.infinite.reactnativemlkit.example",
-    adaptiveIcon: {
-      foregroundImage: "./assets/images/app-icon-android-adaptive-foreground.png",
-      backgroundImage: "./assets/images/app-icon-android-adaptive-background.png",
-      backgroundColor: "#F4F2F1",
-    },
-    splash: {
-      image: "./assets/images/splash-logo-android-universal.png",
-      resizeMode: "contain",
-      backgroundColor: "#F4F2F1",
-    },
-    versionCode: BUILD_NUMBER,
-  },
-  ios: {
-    icon: "./assets/images/app-icon-ios.png",
-    supportsTablet: true,
-    bundleIdentifier: "red.infinite.reactnativemlkit.example",
-    splash: {
-      image: "./assets/images/splash-logo-ios-mobile.png",
-      tabletImage: "./assets/images/splash-logo-ios-tablet.png",
-      resizeMode: "contain",
-      backgroundColor: "#F4F2F1",
-    },
-    buildNumber: String(BUILD_NUMBER),
-  },
-  web: {
-    favicon: "./assets/images/app-icon-web-favicon.png",
-    splash: {
-      image: "./assets/images/splash-logo-web.png",
-      resizeMode: "contain",
-      backgroundColor: "#F4F2F1",
-    },
-    bundler: "metro",
-  },
-  plugins: [
-    "expo-localization",
-    [
-      "expo-build-properties",
-      {
-        ios: {
-          newArchEnabled: false,
-          flipper: false,
-          deploymentTarget: "13.4",
-        },
-        android: {
-          newArchEnabled: false,
-          compileSdkVersion: 34,
-        },
-      },
-    ],
-    "expo-font",
-    withSplashScreen,
-    withFlipperDisabled,
-  ],
-  experiments: {
-    tsconfigPaths: true,
-  },
-})
+module.exports = ({ config }: ConfigContext): Partial<ExpoConfig> => {
+  const existingPlugins = config.plugins ?? []
+  return {
+    ...config,
+    plugins: [...existingPlugins, withSplashScreen, withFlipperDisabled],
+  }
+}
