2e0e89561167cbf6489fbf446677b7b9357994c7
Aiden Petersen <aiden.petersen@mattr.global>
2025-01-25 11:28:43 +1300

    chore(react-native-mlkit-object-detection): remove deprecated event emitter

--- modules/react-native-mlkit-object-detection/src/RNMLKitObjectDetectionModule.web.ts
+++ modules/react-native-mlkit-object-detection/src/RNMLKitObjectDetectionModule.web.ts
@@ -1,12 +1,6 @@
-import { EventEmitter } from "expo-modules-core";
-
-const emitter = new EventEmitter({} as any);
-
 export default {
   PI: Math.PI,
-  async setValueAsync(value: string): Promise<void> {
-    emitter.emit("onChange", { value });
-  },
+  async setValueAsync(value: string): Promise<void> {},
   hello() {
     return "Hello world! 👋";
   },
