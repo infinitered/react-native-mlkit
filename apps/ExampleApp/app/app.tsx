17d4511b47ef71c3869eb340cd3be1ad3376c8e0
Trevor Coleman <trevor@infinite.red>
2025-02-06 16:02:13 -0500

    feat: Upgrade for Expo v52 compatibility (#183)
    * fix: remove obsolete plugins
    
    * fix: fix variable assignment
    
    Co-authored-by: Aiden Petersen <aiden.petersen@gmail.com>

--- apps/ExampleApp/app/app.tsx
+++ apps/ExampleApp/app/app.tsx
@@ -55,7 +55,7 @@ const config = {
 }
 
 interface AppProps {
-  hideSplashScreen: () => Promise<boolean>
+  hideSplashScreen: () => Promise<void>
 }
 
 /**
@@ -76,10 +76,12 @@ function App(props: AppProps) {
   const { rehydrated } = useInitialRootStore(() => {
     // This runs after the root store has been initialized and rehydrated.
 
-    // If your initialization scripts run very fast, it's good to show the splash screen for just a bit longer to prevent flicker.
-    // Slightly delaying splash screen hiding for better UX; can be customized or removed as needed,
-    // Note: (vanilla Android) The splash-screen will not appear if you launch your app via the terminal or Android Studio. Kill the app and launch it normally by tapping on the launcher icon. https://stackoverflow.com/a/69831106
-    // Note: (vanilla iOS) You might notice the splash-screen logo change size. This happens in debug/development mode. Try building the app for release.
+    // If your initialization scripts run very fast, it's good to show the splash screen for just a bit longer to
+    // prevent flicker. Slightly delaying splash screen hiding for better UX; can be customized or removed as needed,
+    // Note: (vanilla Android) The splash-screen will not appear if you launch your app via the terminal or Android
+    // Studio. Kill the app and launch it normally by tapping on the launcher icon.
+    // https://stackoverflow.com/a/69831106 Note: (vanilla iOS) You might notice the splash-screen logo change size.
+    // This happens in debug/development mode. Try building the app for release.
     setTimeout(hideSplashScreen, 500)
   })
 
