b95acd943aadc8bc6b0d105bfb7210ea3b541844
Tyler Williams <tyler@coolsoftware.dev>
2025-12-05 18:49:59 -0500

    feat: finish barcode types on TS side

--- modules/react-native-mlkit-barcode-scanning/src/ReactNativeMlkitBarcodeScanning.types.ts
+++ modules/react-native-mlkit-barcode-scanning/src/ReactNativeMlkitBarcodeScanning.types.ts
@@ -18,6 +18,25 @@ export enum BarcodeFormat {
   aztec = "AZTEC",
 }
 
+/**
+ * Possible value types. Taken from Android API reference:
+ * https://developers.google.com/android/reference/com/google/mlkit/vision/barcode/common/Barcode.BarcodeValueType?hl=en
+ */
+export enum BarcodeValueType {
+  UNKNOWN = "UNKNOWN",
+  CONTACT_INFO = "CONTACT_INFO",
+  EMAIL = "EMAIL",
+  ISBN = "ISBN",
+  PHONE = "PHONE",
+  PRODUCT = "PRODUCT",
+  SMS = "SMS",
+  TEXT = "TEXT",
+  URL = "URL",
+  WIFI = "WIFI",
+  GEO = "GEO",
+  DRIVER_LICENSE = "DRIVER_LICENSE",
+  CALENDAR_EVENT = "CALENDAR_EVENT",
+}
 
 export interface BarcodeScannerOptions {
   formats: BarcodeFormat[]
@@ -35,6 +54,134 @@ export interface BarcodeScannerResult {
  */
 export type BarcodeRawData = Uint8Array[];
 
+/**
+ * Possible email types. Taken from Android API reference:
+ * https://developers.google.com/android/reference/com/google/mlkit/vision/barcode/common/Barcode.Email.FormatType
+ */
+export enum BarcodeEmailType {
+  HOME = "HOME",
+  WORK = "WORK",
+  UNKNOWN = "UNKNOWN",
+}
+
+export interface BarcodeEmail {
+  address?: string
+  body?: string
+  subject?: string
+  type: BarcodeEmailType
+}
+
+/**
+ * Possible phone types. Taken from Android API reference:
+ * https://developers.google.com/android/reference/com/google/mlkit/vision/barcode/common/Barcode.Phone.FormatType
+ */
+export enum BarcodePhoneType {
+  UNKNOWN = "UNKNOWN",
+  WORK = "WORK",
+  HOME = "HOME",
+  FAX = "FAX",
+  MOBILE = "MOBILE"
+}
+
+export interface BarcodePhone {
+  number?: string
+  type: BarcodePhoneType
+}
+
+export interface BarcodeSMS {
+  message?: string
+  phoneNumber?: string
+}
+
+export interface BarcodeURLBookmark {
+  title?: string
+  url?: string
+}
+
+/**
+ * https://developers.google.com/android/reference/com/google/mlkit/vision/barcode/common/Barcode.WiFi.EncryptionType
+ */
+export enum BarcodeWifiEncryptionType {
+  OPEN = "OPEN",
+  WPA = "WPA",
+  WEP = "WEP"
+}
+
+export interface BarcodeWifi {
+  ssid?: string
+  password?: string
+  type: BarcodeWifiEncryptionType 
+}
+
+export interface BarcodeGeoPoint {
+  latitude: number
+  longitude: number
+}
+
+/**
+ * https://developers.google.com/android/reference/com/google/mlkit/vision/barcode/common/Barcode.Address.AddressType
+ */
+export enum BarcodeAddressType {
+  UNKNOWN = "UNKNOWN",
+  WORK = "WORK",
+  HOME = "HOME",
+}
+
+export interface BarcodeAddress {
+  addressLines?: Array<string>
+  type: BarcodeAddressType
+}
+
+export interface BarcodePersonName {
+  formattedName?: string
+  first?: string
+  last?: string
+  middle?: string
+  prefix?: string
+  pronunciation?: string
+  suffix?: string
+}
+
+export interface BarcodeContactInfo {
+  addresses?: Array<BarcodeAddress>
+  emails?: Array<BarcodeEmail>
+  name?: BarcodePersonName
+  phones?: Array<BarcodePhone>
+  urls?: Array<string>
+  jobTitle?: string
+  organization?: string
+}
+
+export interface BarcodeCalendarEvent {
+  eventDescription?: string
+  location?: string
+  organizer?: string
+  status?: string
+  summary?: string
+  start?: number // We use an epoch millisecond time stamp to represent the Date field here
+  end?: number // We use an epoch millisecond time stamp to represent the Date field here
+}
+
+/**
+ * DL for driver's license, ID for ID cards
+ */
+export type BarcodeDriverLicenseDocumentType = 'DL' | 'ID'
+
+export interface BarcodeDriverLicense {
+  firstName?: string
+  middleName?: string
+  lastName?: string
+  gender?: string
+  addressCity?: string
+  addressState?: string
+  addressStreet?: string
+  addressZip?: string
+  birthDate?: string
+  documentType?: BarcodeDriverLicenseDocumentType
+  expiryDate?: string
+  issuingDate?: string
+  issuingCountry?: string
+}
 
 export interface Barcode {
   frame: RNMLKitRect
