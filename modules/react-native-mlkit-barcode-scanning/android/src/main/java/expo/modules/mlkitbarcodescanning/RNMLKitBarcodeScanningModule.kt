c4465ef9d435462a5c1b4f314481b13b75d26207
Tyler Williams <tyler@coolsoftware.dev>
2025-12-05 19:09:04 -0500

    refactor: rename files to RNMLKit

--- modules/react-native-mlkit-barcode-scanning/android/src/main/java/expo/modules/mlkitbarcodescanning/RNMLKitBarcodeScanningModule.kt
+++ modules/react-native-mlkit-barcode-scanning/android/src/main/java/expo/modules/mlkitbarcodescanning/RNMLKitBarcodeScanningModule.kt
@@ -1,4 +1,4 @@
-package expo.modules.mlkitbarcodescanning
+package red.infinite.reactnativemlkit
 
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
--- modules/react-native-mlkit-barcode-scanning/index.ts
+++ modules/react-native-mlkit-barcode-scanning/index.ts
@@ -1,4 +1,4 @@
 // Reexport the native module. On web, it will be resolved to RNMLKitBarcodeScanningModule.web.ts
 // and on native platforms to RNMLKitBarcodeScanningModule.ts
-export { default } from './src/RNMLKitBarcodeScanningModule';
-export * from  './src/RNMLKitBarcodeScanning.types';
+export { default } from "./src/RNMLKitBarcodeScanningModule";
+export * from "./src/RNMLKitBarcodeScanning.types";
