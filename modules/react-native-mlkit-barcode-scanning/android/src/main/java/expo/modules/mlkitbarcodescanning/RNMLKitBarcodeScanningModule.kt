217f0e7266724c7e5025de011606f92be8771a80
Frank Calise <fcalise@gmail.com>
2025-12-15 16:46:47 -0500

    fix(barcode-scanning): android builds

--- modules/react-native-mlkit-barcode-scanning/android/src/main/java/expo/modules/mlkitbarcodescanning/RNMLKitBarcodeScanningModule.kt
+++ modules/react-native-mlkit-barcode-scanning/android/src/main/java/expo/modules/mlkitbarcodescanning/RNMLKitBarcodeScanningModule.kt
@@ -35,16 +35,5 @@ class RNMLKitBarcodeScanningModule : Module() {
         "value" to value
       ))
     }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(RNMLKitBarcodeScanningView::class) {
-      // Defines a setter for the `url` prop.
-      Prop("url") { view: RNMLKitBarcodeScanningView, url: URL ->
-        view.webView.loadUrl(url.toString())
-      }
-      // Defines an event that the view can send to JavaScript.
-      Events("onLoad")
-    }
   }
 }
