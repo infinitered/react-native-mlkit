03f7fe4b445751c856bcfbddf176fe1bf5b34895
Tyler Williams <tyler@coolsoftware.dev>
2025-12-18 10:13:21 -0500

    feat: working, with claude-generated swift types

--- modules/react-native-mlkit-barcode-scanning/ios/RNMLKitBarcodeScanningModule.swift
+++ modules/react-native-mlkit-barcode-scanning/ios/RNMLKitBarcodeScanningModule.swift
@@ -1,11 +1,54 @@
 import ExpoModulesCore
+import MLKitBarcodeScanning
+import RNMLKitCore
+
+// TODO: move this to core?
+// Function to reject a promise with a specified message and domain
+func rejectPromiseWithMessage(promise: Promise, message: String, domain: String)
+{
+    promise.reject(
+        NSError(
+            domain: domain, code: 1,
+            userInfo: [NSLocalizedDescriptionKey: message])
+    )
+}
+
+let ERROR_DOMAIN: String =
+    "red.infinite.reactnativemlkit.BarcodeScanningErrorDomain"
 
 public class RNMLKitBarcodeScanningModule: Module {
-  public func definition() -> ModuleDefinition {
-    Name("RNMLKitBarcodeScanning")
+    // TODO: set up ways to initialize and change this
+    var barcodeScanner: BarcodeScanner? = BarcodeScanner.barcodeScanner()
 
-    AsyncFunction("process") { (value: String) in
-      print(value)
+    public func definition() -> ModuleDefinition {
+        Name("RNMLKitBarcodeScanning")
+
+        AsyncFunction("process") { (imagePath: String, promise: Promise) in
+            let logger = Logger(logHandlers: [
+                createOSLogHandler(category: Logger.EXPO_LOG_CATEGORY)
+            ])
+
+            Task {
+                do {
+                    guard let barcodeScanner = self.barcodeScanner else {
+                        rejectPromiseWithMessage(
+                            promise: promise,
+                            message:
+                                "[RNMLKitBarcodeScanning.process] Barcode Scanner not initiliazed",
+                            domain: ERROR_DOMAIN)
+                        return
+                    }
+                    let image = try RNMLKitImage(imagePath: imagePath)
+                    let barcodes = try await barcodeScanner.process(image.visionImage)
+
+                    logger.debug(barcodes)
+
+                    let result = RNMLKitBarcodeScannerResult(barcodes: barcodes)
+                    promise.resolve(result.record)
+                } catch {
+                    rejectPromiseWithMessage(promise: promise, message: "[RNMLKitBarcodeScanning.process] Error processing barcode: \(error)", domain: ERROR_DOMAIN)
+                }
+            }
+        }
     }
-  }
 }
